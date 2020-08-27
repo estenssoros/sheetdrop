66c81f4054af7353516db4aa38e3cb640baa4dc1
estenssoros <seb.estenssor@gmail.com>
2020-08-27 08:07:41 -0600

    login handler

--- internal/server/api.go
+++ internal/server/api.go
@@ -3,12 +3,22 @@ package server
 import (
 	"net/http"
 
+	"github.com/estenssoros/sheetdrop/constants"
 	"github.com/estenssoros/sheetdrop/controllers"
-	"github.com/labstack/echo"
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func routes(e *echo.Group) {
 	e.POST("/file-upload", fileUploadHandler)
+	e.POST("/login", loginHandler, middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "form:csrf",
+	}))
+	e.GET("/logout", logoutHandler, middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "form:csrf",
+	}))
 }
 
 func fileUploadHandler(c echo.Context) error {
@@ -20,13 +30,38 @@ func fileUploadHandler(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err)
 	}
-	resp, err := controllers.ProcessFile(&controllers.ProcessFileInput{
+
+	resp, err := controllers.ProcessFile(c.Get(constants.ContextDB).(*gorm.DB), &controllers.ProcessFileInput{
 		FileName: file.Filename,
 		File:     multiPart,
-		Size:     file.Size,
+		User:     c.FormValue("user"),
 	})
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
+		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusOK, resp)
+}
+
+func loginHandler(c echo.Context) error {
+	input := &controllers.LoginInput{}
+	if err := c.Bind(input); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	if err := controllers.Login(c.Get(constants.ContextDB).(*gorm.DB), input); err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	sess, _ := session.Get("session", c)
+	sess.Values[constants.SessionLoggedIn] = true
+	sess.Values[constants.SessionUser] = *input.UserName
+	sess.Save(c.Request(), c.Response())
+	loggedIn, _ := sess.Values[constants.SessionLoggedIn].(bool)
+	return c.Render(http.StatusOK, "main", struct{ LoggedIn bool }{loggedIn})
+}
+
+func logoutHandler(c echo.Context) error {
+	sess, _ := session.Get("session", c)
+	sess.Values[constants.SessionLoggedIn] = true
+	delete(sess.Values, constants.SessionUser)
+	sess.Save(c.Request(), c.Response())
+	return c.Render(http.StatusOK, "login", c.Get("csrf").(string))
 }
