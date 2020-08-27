e8f2ff07f586033b07ae35a0060b41fa7ce620f0
estenssoros <seb.estenssor@gmail.com>
2020-08-27 08:07:32 -0600

    lots of middleware

--- internal/server/cmd.go
+++ internal/server/cmd.go
@@ -3,10 +3,16 @@ package server
 import (
 	"net/http"
 
+	"github.com/estenssoros/sheetdrop/constants"
+	"github.com/estenssoros/sheetdrop/controllers"
 	"github.com/estenssoros/sheetdrop/internal/middle"
 	"github.com/estenssoros/sheetdrop/orm"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/estenssoros/sheetdrop/responses"
+	"github.com/gorilla/sessions"
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -26,25 +32,47 @@ func run() error {
 		return errors.Wrap(err, "NewTemplate")
 	}
 	e.Renderer = t
-	e.GET("/", Main)
 	db, err := orm.Connect()
 	if err != nil {
 		return errors.Wrap(err, "orm.Connect")
 	}
 	defer db.Close()
-	e.Use(middleware.Logger())
+	e.Use(middle.DBInjector(db))
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+	e.GET("/", main, middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "form:csrf",
+	}))
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}, error=${error}\n",
+	}))
 	e.Use(middleware.Recover())
 	e.Use(middleware.CORS())
 	routes(e.Group(
 		"/api",
 		// middleware.JWT([]byte("secret")),
-		middle.DBInjector(db),
 	))
 
 	return e.Start(":1323")
 }
 
 // Main main template
-func Main(c echo.Context) error {
-	return c.Render(http.StatusOK, "main", "World")
+func main(c echo.Context) error {
+	sess, _ := session.Get("session", c)
+	loggedIn, ok := sess.Values[constants.SessionLoggedIn].(bool)
+	if ok && loggedIn {
+		user, ok := sess.Values[constants.SessionUser].(string)
+		if !ok {
+			return c.JSON(http.StatusInternalServerError, "no session user")
+		}
+		apis, err := controllers.GetUserAPIs(c.Get(constants.ContextDB).(*gorm.DB), user)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		return c.Render(http.StatusOK, "main", &responses.Main{
+			LoggedIn: loggedIn,
+			APIs:     apis,
+		})
+	}
+	return c.Render(http.StatusOK, "login", c.Get("csrf").(string))
 }
