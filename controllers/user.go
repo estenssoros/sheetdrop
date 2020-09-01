f955b0052d34b65f12473f215ceca61dbea53d4a
estenssoros <seb.estenssor@gmail.com>
2020-09-01 11:38:10 -0600

    feat: user controllers

--- controllers/user.go
+++ controllers/user.go
@@ -9,3 +9,13 @@ func GetUserByName(db *gorm.DB, userName string) (*models.User, error) {
 	user := &models.User{}
 	return user, db.Where("user_name=?", userName).First(user).Error
 }
+
+func GetOrCreateUserByName(db *gorm.DB, userName string) (*models.User, error) {
+	user := &models.User{}
+	return user, db.Where(models.User{UserName: userName}).FirstOrCreate(user).Error
+}
+
+func GetUserByID(db *gorm.DB, userID int) (*models.User, error) {
+	user := &models.User{}
+	return user, db.Where("id=?", userID).First(user).Error
+}
