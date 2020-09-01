844a59ff15b045a6afe3b0c05f5f3d994b64147e
estenssoros <seb.estenssor@gmail.com>
2020-09-01 11:38:29 -0600

    api deleted  at

--- internal/models/api.go
+++ internal/models/api.go
@@ -1,12 +1,16 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 // API api endpoint for a user
 type API struct {
 	ID        int
 	CreatedAt time.Time
 	UpdatedAt time.Time
+	DeletedAt *time.Time `json:"-"`
 	UserID    int
 	Name      *string `gorm:"type:varchar(50)"`
 }
@@ -14,4 +18,9 @@ type API struct {
 // TableName implements tablenameable
 func (a API) TableName() string {
 	return `api`
+}
+
+func (a API) String() string {
+	ju, _ := json.MarshalIndent(a, "", " ")
+	return string(ju)
 }
