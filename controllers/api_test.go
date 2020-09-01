216dfcf21c74575c37b5eb6aef8cc5be2b805412
estenssoros <seb.estenssor@gmail.com>
2020-09-01 11:37:21 -0600

    test api

---
+++ controllers/api_test.go
@@ -0,0 +1,21 @@
+package controllers
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/estenssoros/sheetdrop/orm"
+)
+
+func TestGetUserFromAPIID(t *testing.T) {
+	db, err := orm.Connect()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+	user, err := GetUserFromAPIID(db, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(user)
+}
