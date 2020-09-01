302f59367d10a56896949b70351dc43e680d4de1
estenssoros <seb.estenssor@gmail.com>
2020-09-01 11:37:35 -0600

    API actions

--- controllers/api.go
+++ controllers/api.go
@@ -3,14 +3,24 @@ package controllers
 import (
 	"github.com/estenssoros/sheetdrop/internal/models"
 	"github.com/jinzhu/gorm"
-	"github.com/pkg/errors"
 )
 
-func GetUserAPIs(db *gorm.DB, userName string) ([]*models.API, error) {
+func GetUserAPIs(db *gorm.DB, user *models.User) ([]*models.API, error) {
 	apis := []*models.API{}
-	user, err := GetUserByName(db, userName)
-	if err != nil {
-		return nil, errors.Wrap(err, "GetUserByName")
-	}
 	return apis, db.Where("user_id=?", user.ID).Find(&apis).Error
+}
+
+func CreateAPIForUser(db *gorm.DB, user *models.User) (*models.API, error) {
+	api := &models.API{UserID: user.ID}
+	return api, db.Create(api).Error
+}
+
+func GetAPIByID(db *gorm.DB, id int) (*models.API, error) {
+	api := &models.API{}
+	return api, db.Where("id=?", id).First(api).Error
+}
+
+func GetUserFromAPIID(db *gorm.DB, apiID int) (*models.User, error) {
+	user := &models.User{}
+	return user, db.Model(&models.API{}).Joins("JOIN api ON api.user_id = user.id").Where("api.id=?", apiID).First(user).Error
 }
