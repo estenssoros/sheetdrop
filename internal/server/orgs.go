2005da1ce499fd7fd1260c42b59f046c5aa99236
estenssoros <seb.estenssor@gmail.com>
2020-09-16 07:21:38 -0600

    feat: org methods

--- internal/server/orgs.go
+++ internal/server/orgs.go
@@ -5,19 +5,89 @@ import (
 
 	"github.com/estenssoros/sheetdrop/constants"
 	"github.com/estenssoros/sheetdrop/controllers"
+	"github.com/estenssoros/sheetdrop/models"
 	"github.com/labstack/echo/v4"
 )
 
 func getOrgsHandler(c echo.Context) error {
 	userName := c.Get(constants.ContextUserName).(string)
-	ctl := c.Get(constants.ContextDB).(controllers.Interface)
+	ctl := extractController(c)
 	user, err := ctl.GetOrCreateUserByName(userName)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
-	orgs, err := ctl.GetUserOrgs(user)
+	orgs, err := ctl.GetUserOrgsResponse(user)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusOK, orgs)
+}
+
+func createOrgHandler(c echo.Context) error {
+	model := &models.Organization{}
+	if err := c.Bind(model); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	if model.Name == nil {
+		return c.JSON(http.StatusBadRequest, "missing api name")
+	}
+	ctl := extractController(c)
+	user, err := ctl.GetUserByName(c.Get(constants.ContextUserName).(string))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	input := &controllers.CreateOrgInput{
+		Org:  model,
+		User: user,
+	}
+	if err := ctl.CreateOrg(input); err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, model)
+}
+
+func updateOrgHandler(c echo.Context) error {
+	model := &models.Organization{}
+	if err := c.Bind(model); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	ctl := extractController(c)
+	user, err := ctl.GetUserByName(extractUserName(c))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	canEdit, err := ctl.UserCanEditOrg(user, model)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	if !canEdit {
+		return c.JSON(http.StatusForbidden, "user cannot edit org")
+	}
+	if err := ctl.UpdateOrg(model); err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, model)
+}
+
+func deleteOrgHandler(c echo.Context) error {
+	model := &models.Organization{}
+	if err := c.Bind(model); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	ctl := extractController(c)
+	user, err := ctl.GetUserByName(extractUserName(c))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	canEdit, err := ctl.UserCanEditOrg(user, model)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	if !canEdit {
+		return c.JSON(http.StatusForbidden, "user cannot edit org")
+	}
+	if err := ctl.DB().Delete(model).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.NoContent(http.StatusOK)
 }
