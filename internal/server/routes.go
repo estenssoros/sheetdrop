575e4a8f83022d724264460e6e6b56865e4da4b1
estenssoros <seb.estenssor@gmail.com>
2020-09-16 07:21:45 -0600

    refactor: route cleanup

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,14 +5,27 @@ import (
 )
 
 func routes(e *echo.Group) {
+	apiRoutes(e)
+	orgRoutes(e)
+	schemaRoutes(e)
+}
+
+func apiRoutes(e *echo.Group) {
 	e.GET("/apis", getAPIsHandler)
 	e.GET("/api/:id", getAPIHandler)
 	e.POST("/api", createAPIHandler)
 	e.DELETE("/api", deleteAPIHandler)
 	e.PATCH("/api", updateAPIHandler)
+}
 
+func orgRoutes(e *echo.Group) {
 	e.GET("/orgs", getOrgsHandler)
+	e.POST("/org", createOrgHandler)
+	e.PATCH("/org", updateOrgHandler)
+	e.DELETE("/org", deleteOrgHandler)
+}
 
+func schemaRoutes(e *echo.Group) {
 	e.GET("/schema/:apiID", getSchemaHandler)
 	e.PATCH("/schema", updateSchemaHandler)
 	e.DELETE("/schema", deleteSchemaHandler)
