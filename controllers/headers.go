fd21526ee4130274929e94e416bdd62f83042fca
estenssoros <seb.estenssoro@gmail.com>
2020-12-27 08:48:47 -0700

    feat: header foreign keys

--- controllers/headers.go
+++ controllers/headers.go
@@ -1,1 +1,12 @@
 package controllers
+
+import "github.com/estenssoros/sheetdrop/models"
+
+// HeaderForeignKeys finds headers with matching foreign keys
+func (c *Controller) HeaderForeignKeys(headerID int) ([]*models.Header, error) {
+	headers := []*models.Header{}
+	query := c.Model(&models.HeaderHeader{}).
+		Select("foreign_header_id").
+		Where("header_id=?", headerID)
+	return headers, c.Where("id IN (?)", query).Find(&headers).Error
+}
