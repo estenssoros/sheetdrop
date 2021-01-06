9ca8ef261705f66a822fba98a44c07efefc9d250
estenssoros <seb.estenssoro@gmail.com>
2021-01-05 20:57:30 -0700

    test: validator

--- controllers/controllers_test.go
+++ controllers/controllers_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/estenssoros/sheetdrop/orm"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
 func ctl(t *testing.T) *Controller {
@@ -12,4 +14,14 @@ func ctl(t *testing.T) *Controller {
 		t.Fatal(err)
 	}
 	return New(db)
+}
+
+type validator struct{}
+
+func (v validator) Validate(db *gorm.DB) error {
+	return nil
+}
+func TestValidator(t *testing.T) {
+	assert.Nil(t, ctl(t).Validate(&validator{}))
+	assert.Nil(t, ctl(t).Validate(7))
 }
