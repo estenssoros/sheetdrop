d16a0a2e8df7868a97627316bffd411797e5374d
estenssoros <seb.estenssoro@gmail.com>
2021-01-05 20:58:50 -0700

    patch: not used

--- server/orgs.go
+++ server/orgs.go
@@ -3,7 +3,6 @@ package server
 import (
 	"net/http"
 
-	"github.com/estenssoros/sheetdrop/controllers"
 	"github.com/estenssoros/sheetdrop/models"
 	"github.com/labstack/echo/v4"
 )
@@ -20,27 +19,27 @@ func getOrgsHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, orgs)
 }
 
-func createOrgHandler(c echo.Context) error {
-	model := &models.Organization{}
-	if err := c.Bind(model); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
-	}
-	if model.Name == nil {
-		return c.JSON(http.StatusBadRequest, "missing api name")
-	}
-	user, err := ctl(c).GetUserByName(usr(c))
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-	input := &controllers.CreateOrgInput{
-		Org:  model,
-		User: user,
-	}
-	if err := ctl(c).CreateOrg(input); err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-	return c.JSON(http.StatusOK, model)
-}
+// func createOrgHandler(c echo.Context) error {
+// 	model := &models.Organization{}
+// 	if err := c.Bind(model); err != nil {
+// 		return c.JSON(http.StatusBadRequest, err.Error())
+// 	}
+// 	if model.Name == nil {
+// 		return c.JSON(http.StatusBadRequest, "missing api name")
+// 	}
+// 	user, err := ctl(c).GetUserByName(usr(c))
+// 	if err != nil {
+// 		return c.JSON(http.StatusInternalServerError, err.Error())
+// 	}
+// 	input := &controllers.CreateOrgInput{
+// 		Org:  model,
+// 		User: user,
+// 	}
+// 	if err := ctl(c).CreateOrg(input); err != nil {
+// 		return c.JSON(http.StatusInternalServerError, err.Error())
+// 	}
+// 	return c.JSON(http.StatusOK, model)
+// }
 
 func updateOrgHandler(c echo.Context) error {
 	org := &models.Organization{}
