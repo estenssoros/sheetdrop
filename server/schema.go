502e2bafe3b8f76dbeb85fb6b015fc34174f16ea
estenssoros <seb.estenssoro@gmail.com>
2021-01-05 20:59:06 -0700

    patch: not used

--- server/schema.go
+++ server/schema.go
@@ -6,9 +6,7 @@ import (
 
 	"github.com/estenssoros/sheetdrop/controllers"
 	"github.com/estenssoros/sheetdrop/models"
-	"github.com/estenssoros/sheetdrop/responses"
 	"github.com/labstack/echo/v4"
-	"github.com/pkg/errors"
 )
 
 func getSchemaHandler(c echo.Context) error {
@@ -71,41 +69,41 @@ func deleteSchemaHandler(c echo.Context) error {
 	return c.NoContent(http.StatusOK)
 }
 
-func schemaFilePatchHandler(c echo.Context) error {
-	input := &controllers.ProcessFileInput{}
-	if err := c.Bind(input); err != nil {
-		return responses.Error(c, http.StatusBadRequest, errors.Wrap(err, "c.Bind"))
-	}
-	input.User = usr(c)
-	return fileUploadHandler(c, input)
-}
+// func schemaFilePatchHandler(c echo.Context) error {
+// 	input := &controllers.ProcessFileInput{}
+// 	if err := c.Bind(input); err != nil {
+// 		return responses.Error(c, http.StatusBadRequest, errors.Wrap(err, "c.Bind"))
+// 	}
+// 	input.User = usr(c)
+// 	return fileUploadHandler(c, input)
+// }
 
-func schemaFileUploadHandler(c echo.Context) error {
-	input := &controllers.ProcessFileInput{}
-	if err := c.Bind(input); err != nil {
-		return responses.Error(c, http.StatusBadRequest, errors.Wrap(err, "c.Bind"))
-	}
-	input.User = usr(c)
-	input.NewSchema = true
-	return fileUploadHandler(c, input)
-}
+// func schemaFileUploadHandler(c echo.Context) error {
+// 	input := &controllers.ProcessFileInput{}
+// 	if err := c.Bind(input); err != nil {
+// 		return responses.Error(c, http.StatusBadRequest, errors.Wrap(err, "c.Bind"))
+// 	}
+// 	input.User = usr(c)
+// 	input.NewSchema = true
+// 	return fileUploadHandler(c, input)
+// }
 
-func fileUploadHandler(c echo.Context, input *controllers.ProcessFileInput) error {
-	file, err := c.FormFile("file")
-	if err != nil {
-		return responses.Error(c, http.StatusBadRequest, errors.Wrap(err, "c.FormFile"))
-	}
-	input.FileName = file.Filename
+// func fileUploadHandler(c echo.Context, input *controllers.ProcessFileInput) error {
+// 	file, err := c.FormFile("file")
+// 	if err != nil {
+// 		return responses.Error(c, http.StatusBadRequest, errors.Wrap(err, "c.FormFile"))
+// 	}
+// 	input.FileName = file.Filename
 
-	multiPart, err := file.Open()
-	if err != nil {
-		return responses.Error(c, http.StatusInternalServerError, errors.Wrap(err, "file.Open"))
-	}
+// 	multiPart, err := file.Open()
+// 	if err != nil {
+// 		return responses.Error(c, http.StatusInternalServerError, errors.Wrap(err, "file.Open"))
+// 	}
 
-	input.File = multiPart
-	resp, err := ctl(c).ProcessFile(input)
-	if err != nil {
-		return responses.Error(c, http.StatusInternalServerError, errors.Wrap(err, "controllers.ProcessFile"))
-	}
-	return c.JSON(http.StatusOK, resp)
-}
+// 	input.File = multiPart
+// 	resp, err := ctl(c).ProcessFile(input)
+// 	if err != nil {
+// 		return responses.Error(c, http.StatusInternalServerError, errors.Wrap(err, "controllers.ProcessFile"))
+// 	}
+// 	return c.JSON(http.StatusOK, resp)
+// }
